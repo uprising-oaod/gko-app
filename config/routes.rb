d7b11b4c078a47c8b951729211448c91e6110697
danner2 <dennismorosoff@gmail.com>
2022-01-24 08:31:14 +0300

    #3 Добавил страницу, чтобы посмотреть как накатился Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/dashboard', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
+
+  get 'welcome/index'
 end
