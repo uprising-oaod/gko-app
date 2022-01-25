ff418a0d1b0948bc89b82d3b0b030db1fcbfff63
danner2 <dennismorosoff@gmail.com>
2022-01-25 13:06:45 +0300

    #6 Исправлена ошибка рефакторинга

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/health', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/dashboard', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
 
