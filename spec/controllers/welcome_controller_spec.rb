3365b3131f61d4dd1fed3f4b92bb2791ff6bbb69
danner2 <dennismorosoff@gmail.com>
2022-01-28 07:18:10 +0300

    #11 Добавил тест для демонстрационного контроллера

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do
+  describe "GET index" do
+    it "returns a 200" do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
