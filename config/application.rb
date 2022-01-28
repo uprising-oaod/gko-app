7e4ac0a0c1a2d86df3f88715010b2b1f643179af
danner2 <dennismorosoff@gmail.com>
2022-01-28 07:17:40 +0300

    #11 Добавил настройки для гемов FactoryBot и ShouldaMatcher

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,6 @@ module GkoApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators.test_framework = :rspec
   end
 end
