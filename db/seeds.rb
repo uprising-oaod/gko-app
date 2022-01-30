16b5d11b503f4e5d4a25047a0c7c680096edf858
danner2 <dennismorosoff@gmail.com>
2022-01-30 07:23:16 +0300

    #17 Настроено базовое сидирование

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def yaml_seed(model, caption)
+  seed_file = Rails.root.join('db', 'seeds', "#{caption}.yml")
+  list = YAML::load_file(seed_file)
+  created_items = list[caption].map do |values|
+    model.find_or_create_by!(values)
+  end
+  puts "Created #{created_items.size} #{model} values"
+end
