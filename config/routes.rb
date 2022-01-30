774d5f866ff942340d9db556b0d13a91ace54afd
danner2 <dennismorosoff@gmail.com>
2022-01-30 05:27:41 +0300

    #13 Сконфигурировал гем Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/dashboard', as: 'rails_admin'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
+  get 'welcome/index', as: 'user_root'
 
+  mount RailsAdmin::Engine => '/dashboard', as: 'rails_admin'
+
+  devise_for :users
   get 'welcome/index'
   get 'health/logs'
 end
