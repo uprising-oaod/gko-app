ada0f960cc6bbfab2f85afb6c94bba0375d84364
danner2 <dennismorosoff@gmail.com>
2022-02-02 08:41:25 +0300

    #23 Добавлены данные для сидирования

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 def yaml_seed(model, caption)
   seed_file = Rails.root.join('db', 'seeds', "#{caption}.yml")
@@ -16,3 +11,5 @@ def yaml_seed(model, caption)
 end
 
 yaml_seed(Medal, 'medals')
+yaml_seed(Indicator, 'indicators')
+yaml_seed(AgeGroup, 'age_groups')
